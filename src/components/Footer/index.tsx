2ba4fb47b23df91d1ca543ca96eeee1a03afa561
lucianaTSoares <lucianat.s@hotmail.com>
2022-11-05 04:06:34 -0300

    styles: using new ItemList into Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,26 @@
-import SocialItem from './SocialItem'
+import { Icon } from '@iconify/react'
+import ItemList from '../ItemList'
 import { FooterStyle } from './styles'
+import social from '../../data/social.json'
+import List from '../../styles/icon-list-style'
 
 export default function Footer() {
 	return (
 		<FooterStyle>
 			<div>
-				<p>Veja minhas rede sociais!</p>
-				<ul>
-					<SocialItem path=''>
-				</ul>
+				<p>Veja minhas rede sociais e entre em contato comigo!</p>
+				<List>
+					{social.map((item, key) => (
+						<ItemList
+							key={key}
+							path={item.path}
+							iconColor={item.color}
+							title={item.name}
+						>
+							<Icon icon={item.iconName} />
+						</ItemList>
+					))}
+				</List>
 			</div>
 			<span>2022 - Feito com amor &hearts;</span>
 		</FooterStyle>
