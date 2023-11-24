f1bef64b65fab18e8f7468ae8fc6aa7ec498ea4c
lucianaTSoares <lucianat.s@hotmail.com>
2023-11-23 21:38:24 -0300

    refactor: remove discord social media

--- src/components/SocialMedia/socialMediaList.tsx
+++ src/components/SocialMedia/socialMediaList.tsx
@@ -25,16 +25,6 @@ export const socialMediaList = [
     path: 'https://github.com/lucianaTSoares',
   },
   {
-    name: 'Discord',
-    icon: (
-      <TbBrandDiscord
-        size={iconSize}
-        className='text-zinc-700 transition-all duration-300 hover:text-zinc-400 dark:text-zinc-300 dark:hover:text-zinc-400'
-      />
-    ),
-    path: 'https://discordapp.com/users/302215961736380427',
-  },
-  {
     name: 'E-mail',
     icon: (
       <FiMail
